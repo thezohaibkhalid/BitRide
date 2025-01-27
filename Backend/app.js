5de47ef56adb56beafd63296dd65d50b37a53941
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-01-28 00:34:40 +0500

    updates

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express();
-
+const dotenv = require("dotenv")
+dotenv.config()
+const cors = require("cors");
+app.use(cors())
 app.get("/", (req, res)=>{
     res.send("Hi I am Zohaib")
 })
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const app = require('./app.js');
-
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+const connectToDb =  require("./db/db.js");
 
-server.listen(3000)
+connectToDb();
+server.listen(port, ()=>{
+    console.log('Server is running on port: ' + port);
+})
