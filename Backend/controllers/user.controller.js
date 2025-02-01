16243c5cfb91607a0a6fa4fd3caa9d6055215ab5
Zohaib Khalid <96901150+thezohaibkhalid@users.noreply.github.com>
2025-02-01 22:49:43 +0500

    Update error handling in user.controller.js that is coming from express validator in user routes, for checking all the details, like mail, email etc

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,5 +1,9 @@
 const userModel = require("../models/user.model.js");
-
+const userService = require("../services/user.service")
+const {validationResult} = "express-validator"
 module.exports.registerUser = async (req, res) =>{
-    
+    const errors = validationResult(req);   
+    if (!errors.isEmpty){
+        return res.send(400).json({errors: error.array})
+    }
 }
