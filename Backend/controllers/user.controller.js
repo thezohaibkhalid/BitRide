86be743b4c5b73630a25e9d56b8336af2ed1620a
Zohaib Khalid <96901150+thezohaibkhalid@users.noreply.github.com>
2025-02-02 14:25:55 +0500

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -6,4 +6,19 @@ module.exports.registerUser = async (req, res) =>{
     if (!errors.isEmpty){
         return res.send(400).json({errors: error.array})
     }
+
+    const {firstname, lastname, email, password} = req.body;
+
+    const hashPassword = await userModel.hashPassword(password);
+
+    const user = await userService.createUser({
+        firstname,
+        lastname,
+        email,
+        password:hashPassword,
+    })
+
+    const token = userModel.generateAuthToken( )
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1459613311.
+    res.send({user, token})
 }
