0d7363f91cc48dcbb3b4992cf0d6d4fa6e24dc4c
Zohaib Khalid <96901150+thezohaibkhalid@users.noreply.github.com>
2025-02-02 14:26:23 +0500

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const app = express();
 const dotenv = require("dotenv")
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3436765341.
+const userRoutes = require("../routes/user.routes")
 dotenv.config()
 const cors = require("cors");
+
+
+
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+
 app.get("/", (req, res)=>{
     res.send("Hi I am Zohaib")
 })
+
+app.use("/users", userRoutes)
+
 
 module.exports = app;
